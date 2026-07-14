6e4b57d96d1d62e9d2466e79f169968a1a694ec4
manivannancsmca <manivannancsmca@gmail.com>
2026-07-14 17:52:05 +0530

    first commit

---
+++ src/test/java/com/spring_uuidv7/SpringUuidv7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_uuidv7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUuidv7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
